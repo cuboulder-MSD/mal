3f5ffd3701411a1185d54aebcab0a395e8b322c5
erik radio <etradio1@gmail.com>
2022-05-31 11:47:40 -0600

    Add files via upload

---
+++ av2marc.py
@@ -0,0 +1,77 @@
+import csv, time, sys
+from pymarc import Record, Field
+import datetime
+
+# ts = time.time()
+# st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+
+with open(sys.argv[1], 'rU', encoding='latin-1') as csvFile:
+    reader = csv.DictReader(csvFile)
+    for row in reader:
+        title = row['Name']
+        medium = row['Secondary Type']
+        product = row['Product Name']
+        manufacturer = row['Manufacturer']
+        model = row['Model']
+        year = row['Year']
+        extConn = row['External Connections']
+        intConn = row['Internal Connections']
+        operatingSys = row['Operating System']
+        accessionNo = row['Accession Number']
+
+
+        record = Record()
+        record.add_field(
+            Field(
+                tag = '245',
+                indicators = ['0','0'],
+                subfields = [
+                    'a', title + ' ' + model
+                ]))
+        # record.add_field(
+        #     Field(
+        #         tag = '246',
+        #         indicators = ['0','1'],
+        #         subfields = [
+        #             'a', product + ' ' + model
+        #
+        #         ]))
+        record.add_field(
+            Field(
+                tag = '264',
+                indicators = ['#','3'],
+                subfields = [
+                    'f', manufacturer,
+                    'c', year
+                ]))
+        record.add_field(
+            Field(
+                tag = '500',
+                indicators = ['#','#'],
+                subfields = [
+                    'a', 'Accession Number:' + accessionNo
+                ]))
+        record.add_field(
+            Field(
+                tag = '516',
+                indicators = ['#','#'],
+                subfields = [
+                    'a', medium
+                ]))
+        record.add_field(
+            Field(
+                tag = '793',
+                indicators = ["#","#"],
+                subfields = [
+                    'a', 'Media Archeology Lab (Basement, 1320 Grandview Ave. Boulder CO 80302.)'
+                ]))
+        # record.add_field(
+        #     Field(
+        #         tag = '520',
+        #         indicators = ['#'],
+        #         subfields = [
+        #             'a', operatingSys + '.' + extConn + '.' + intConn + '.'
+        #         ]))
+        print(record)
+        with open('mal_av.dat', 'wb') as out:
+            out.write(record.as_marc())
